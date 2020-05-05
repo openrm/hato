0a487fe6e9222e7d9e930993b6273617ffe395b4
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-05 17:39:31 +0900

    Submit an API proposal

---
+++ examples/index2.js
@@ -0,0 +1,97 @@
+const { Client } = require('../..');
+
+// initialize a client.
+const options = {
+    logger: console
+};
+const client = new Client('amqp://guest:guest@127.0.0.1:5672', options);
+
+// assert some exchanges.
+client
+    .exchange('topic', 'exchange0', { durable: true })
+    .exchange('headers', 'exchange1');
+
+
+//
+// subscriptions
+//
+
+client
+    .subscribe((msg) => {
+        // subscription without parameters results in
+        // listening to `amq.fanout`
+    });
+
+client
+    .bind('a.routing.key')
+    .subscribe((msg) => {
+        // queue with auto-generated name.
+        // listens to the default direct exchange ''
+    });
+
+client
+    .bind('a.topic#')
+    .subscribe((msg) => {
+        // queue with auto-generated name.
+        // listens to `amq.topic`
+    });
+
+client
+    .queue('doSomething') // triggers assertQueue(),
+    .bind('exchange0', 'a.routing.key') // bindQueue() and possibly assertExchange()
+    .subscribe((msg) => {
+        // one can specify the queue name
+        // and bind to a specific exchange as well.
+    });
+
+client
+    // .queue('processFile') // instead of being named here,
+    .bind('exchange0', 'a.routing.key')
+    .subscribe(function processFile(msg) {
+        // a queue can be named by the callback function?
+    });
+
+
+//
+// publications
+//
+
+// somewhere inside callback functions
+async (msg) => {
+    try {
+        // publish to the default unnamed exchange (direct.)
+        await client
+            .publish('a.routing.key', 'message');
+
+        // the routing key will be ignored.
+        await client
+            .exchange('fanout')
+            .publish('a.routing.key', 'message');
+
+        // specify the default topic exchange.
+        await client
+            .exchange('topic')
+            .publish('another.routing.key', { data: 'hello' });
+
+        // assert a custom exchange.
+        await client
+            .exchange('direct', 'my-exchange')
+            .publish('another.routing.key', { data: 'yes' }, { persistent: true });
+
+        // make a rpc.
+        const res = await client
+            .exchange('direct')
+            .rpc('process.file', { file });
+
+        // possibly provide sendToQueue() like this?
+        await client
+            .queue('destination')
+            .publish({ data: 'message' });
+    } catch (err) {
+        // one should catch any exception for retries, fallbacks, etc.
+    }
+}
+
+
+// start serving
+client.start();
