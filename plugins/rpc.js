6faaec37e59eae238075b4f3ad84028b6e65f401
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-12 13:47:11 +0900

    Set timeout for RPCs

--- plugins/rpc.js
+++ plugins/rpc.js
@@ -1,12 +1,13 @@
 const { EventEmitter } = require('events');
 const Plugin = require('./base');
 const { Scopes } = require('../lib/constants');
+const { TimeoutError } = require('../lib/errors');
 
 const Puid = require('puid');
 
 module.exports = class extends Plugin {
 
-    constructor({ uid = new Puid() } = {}) {
+    constructor({ uid = new Puid(), timeout = 0 } = {}) {
         super();
 
         this.wrappers = {
@@ -17,21 +18,38 @@ module.exports = class extends Plugin {
 
                     constructor(...args) {
                         super(...args);
+                        // used to correlate rpc requests and replies
                         this._resp = new EventEmitter();
                     }
 
                     rpc(routingKey, msg, options = {}) {
                         const correlationId = uid.generate();
                         const replyTo = 'amq.rabbitmq.reply-to';
+
                         options = {
                             ...options,
                             correlationId,
                             replyTo
                         };
+
                         const fn = (msg) =>
                             this._resp.emit(msg.properties.correlationId, msg);
+
                         return new Promise((resolve, reject) => {
-                            this._resp.on(correlationId, resolve);
+                            let timer, listener;
+
+                            if (timeout > 0) {
+                                timer = setTimeout(
+                                    () => reject(new TimeoutError(timeout)),
+                                    timeout);
+                            }
+
+                            this._resp.on(correlationId, listener = (msg) => {
+                                timer && clearTimeout(timer);
+                                resolve(msg);
+                                this._resp.removeListener(correlationId, listener);
+                            });
+
                             this.consume(replyTo, fn, { noAck: true })
                                 .then(() => this.publish(routingKey, msg, options))
                                 .catch(reject);
@@ -44,8 +62,12 @@ module.exports = class extends Plugin {
                                 replyTo,
                                 correlationId
                             } = msg.properties;
+
                             const res = fn(msg);
+
+                            // not a rpc
                             if (!replyTo) return res;
+
                             return Promise.resolve(res)
                                 .then((res) => {
                                     return this.publish(replyTo, res, { correlationId });
