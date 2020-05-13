6608c95f396dc057097cee407c6340a82c9f7c8a
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-13 22:36:02 +0900

    Refactor plugin for conn. reattempt

--- plugins/conn-retry.js
+++ plugins/conn-retry.js
@@ -1,45 +1,60 @@
+const Plugin = require('./base');
 const { Scopes: { CONNECTION } } = require('../lib/constants');
 
-module.exports = function(options = {}) {
+const createBackoff = ({
+    min = 1 * 1e3,
+    max = 10 * 1e3,
+    base = 2
+}) => (c) => Math.min(max, Math.pow(base, c) * min);
 
-    const {
-        retries = 5,
-        min = 1 * 1e3,
-        max = 10 * 1e3,
-        factor = 2
-    } = options;
+const retry = ({ retries, backoff, logger, cancelled }) => {
+    return (connect) => {
+        const retryable = (c, ...args) => {
+            if (0 < c) logger.debug('[AMQP:conn-retry] Retrying to connect...');
 
-    this.wrappers = {
-        [CONNECTION]: ({ logger, cancelled }) => (connect) => {
-            cancelled.then((reason) => {
-                logger.warn('[AMQP:conn-retry] Retries will be cancelled. Reason:', reason && reason.message);
-            });
+            return connect(...args)
+                .catch((err) => {
+                    if (c + 1 >= retries) throw err;
 
-            const retryable = (c, ...args) => {
-                if (0 < c) logger.debug('[AMQP:conn-retry] Retrying to connect...');
-                return connect(...args)
-                    .catch((err) => {
-                        if (c + 1 >= retries) throw err;
+                    const wait = backoff(c);
+                    logger.warn(
+                        `[AMQP:conn-retry] Connection failed. Retrying in ${wait}ms...`,
+                        err.message);
 
-                        const wait = Math.min(max, Math.pow(factor, c) * min);
-                        logger.warn(`[AMQP:conn-retry] Connection failed. Retrying in ${wait}ms...`, err.message);
+                    return new Promise((resolve, reject) => {
+                        const id = setTimeout(() =>
+                            retryable(c + 1, ...args).then(resolve).catch(reject), wait);
 
-                        return new Promise((resolve, reject) => {
-                            const id = setTimeout(() =>
-                                retryable(c + 1, ...args).then(resolve).catch(reject), wait);
-
-                            cancelled.then(() => clearTimeout(id));
-                        });
+                        cancelled.then(() => clearTimeout(id));
                     });
-            };
+                });
+        };
 
-            return retryable.bind(null, 0);
-        }
+        return retryable.bind(null, 0);
     };
+};
 
-    this.scopes = Object.keys(this.wrappers);
-    this.wrap = (scope, context) => this.wrappers[scope](context).bind(this);
+module.exports = class extends Plugin {
 
-    return this;
+    constructor(options = {}) {
+        super();
+        this.options = options;
+        this.wrappers = {
+            [CONNECTION]: this.attemptConnection.bind(this)
+        };
+    }
+
+    attemptConnection({ logger, cancelled }) {
+        cancelled.then((reason) => {
+            logger.warn(
+                '[AMQP:conn-retry] Retries will be cancelled. Reason:',
+                reason && reason.message);
+        });
+
+        const { retries = 5 } = this.options;
+        const backoff = createBackoff(this.options);
+
+        return retry({ retries, backoff, logger, cancelled });
+    }
 
 };
