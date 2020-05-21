175e8e7362a6139df34908f553312e8d71a99847
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-21 21:11:00 +0900

    Auto generate service queues

--- plugins/service.js
+++ plugins/service.js
@@ -24,13 +24,35 @@ const associateContext = (resource, service) => (connect) =>
         });
     };
 
+const queueName = (binding, exchange, service) => {
+    let name = `${service.name}:`;
+
+    if (typeof binding === 'string') name += binding;
+    else name += JSON.stringify(binding || null);
+
+    return name += exchange ? `:${exchange}` : '';
+};
+
 module.exports = class ServiceContext extends Plugin {
 
     constructor({ resource, queue = {}, ...service } = {}) {
         super();
 
+        const defaultOptions = queue.options;
         this.wrappers = {
-            [Scopes.CONNECTION]: () => associateContext(resource, service)
+            [Scopes.CONNECTION]: () => associateContext(resource, service),
+            [Scopes.API]: () => (base) => class extends base {
+                consume(binding, fn, options) {
+                    const { queue, exchange } = this._validateContext();
+                    if (queue) return super.consume.apply(this, arguments);
+                    return this
+                        .queue(queueName(binding, exchange, service), {
+                            durable: true,
+                            ...defaultOptions
+                        })
+                        .consume(binding, fn, options);
+                }
+            }
         };
     }
 
