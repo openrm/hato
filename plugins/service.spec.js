80a0f216b769b19c3b32aceeed9334b024e11e7d
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-22 10:42:45 +0900

    Add test

--- plugins/service.spec.js
+++ plugins/service.spec.js
@@ -1,4 +1,4 @@
-const { constants: { Scopes } } = require('..');
+const { Client, constants: { Scopes } } = require('..');
 const ServiceContext = require('./service');
 
 describe('service-context plugin', () => {
@@ -6,7 +6,13 @@ describe('service-context plugin', () => {
         name: 'test-client',
         instanceId: '1234',
         namespace: 'development',
-        version: '0.0.1'
+        version: '0.0.1',
+        queue: {
+            options: {
+                durable: false,
+                exclusive: true
+            }
+        }
     });
     it('injects specified service context into client properties', (done) => {
         const fn = function(url, options) {
@@ -19,5 +25,30 @@ describe('service-context plugin', () => {
             } else done(new Error(`Wrong options returned: ${JSON.stringify(options)}`));
         };
         plugin.wrap(Scopes.CONNECTION)(fn)('', {});
+    });
+    it('names queues automatically in a microservices context', (done) => {
+        const testQueue = {
+            scopes: [Scopes.CHANNEL],
+            wrap() {
+                return (create) => () => create()
+                    .then((ch) => {
+                        const original = ch.assertQueue;
+                        ch.assertQueue = function(name, options) {
+                            if (name === 'test-client:foo:amq.topic'
+                                && !options.durable && options.exclusive) {
+                                done();
+                            } else {
+                                done(new Error('Got wrong queue attributes: name: ' + name +
+                                    ' options: ' + JSON.stringify(options)));
+                            }
+                            return original.apply(this, arguments);
+                        };
+                        return ch;
+                    }).catch(done);
+            }
+        };
+        const client = new Client('amqp://guest:guest@127.0.0.1:5672', { plugins: [plugin, testQueue] });
+        client.type('topic').subscribe('foo', () => {});
+        client.start().catch(done);
     });
 });
