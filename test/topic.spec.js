594a1d5e46e2195a707c21c354def83d0e9c9996
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-05-29 13:30:57 +0900

    Wait for confirmation before closing the channel

--- test/topic.spec.js
+++ test/topic.spec.js
@@ -16,14 +16,14 @@ describe('topic', function() {
                 msg.ack();
 
                 if (content[1] === 'message') {
-                    done();
+                    confirmed.then(() => done());
                 } else {
                     done(new Error("Message not carried properly"));
                 }
             })
             .catch(done);
 
-        client.start()
+        const confirmed = client.start()
             .then(() => client.type('topic')
                 .publish('routing.key.1', Buffer.from(JSON.stringify({ 1: 'message' }))))
             .catch(done);
@@ -44,7 +44,7 @@ describe('topic', function() {
                 msg.ack();
 
                 if (calls === 2) {
-                    done();
+                    confirmed.then(() => done());
                 } else if (calls === 3) {
                     done(new Error("Message received more than twice"));
                 }
@@ -55,7 +55,7 @@ describe('topic', function() {
             .subscribe('routing.key.#', process(1)).catch(done);
 
 
-        client.start()
+        const confirmed = client.start()
             .then(() => client.type('topic').publish('routing.key.1', Buffer.from("1")))
             .then(() => client.type('topic').publish('routing.key.2', Buffer.from("1")))
             .catch(done);
@@ -75,7 +75,7 @@ describe('topic', function() {
                 msg.ack();
 
                 if (calls === 4) {
-                    done();
+                    confirmed.then(() => done());
                 } else if (calls === 2) {
                     done(new Error("Message received twice on the first subscriber"));
                 } else if (calls === 6) {
@@ -90,7 +90,7 @@ describe('topic', function() {
             .subscribe('routing.key.#', process(3)).catch(done);
 
 
-        client.start()
+        const confirmed = client.start()
             .then(() => client.type('topic').publish('routing.key.1', Buffer.from("1")))
             .catch(done);
     });
