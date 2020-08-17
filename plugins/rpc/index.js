26af2f3d92d9aa1dfd41d4a16be3f203eb0a4161
Gensuke Toshiro <toshiro@openrm.co.jp>
2020-08-17 12:22:11 +0900

    Fix duplicate reply queue init

--- plugins/rpc/index.js
+++ plugins/rpc/index.js
@@ -1,6 +1,7 @@
 const Plugin = require('../base');
 const { Scopes } = require('../../lib/constants');
 
+const { EventEmitter } = require('events');
 const Puid = require('puid');
 
 module.exports = class RPCPlugin extends Plugin {
@@ -9,10 +10,29 @@ module.exports = class RPCPlugin extends Plugin {
         super('rpc');
 
         this.options = { uid, timeout };
+
+        this._resp = new EventEmitter();
+        this._configured = false;
     }
 
     init() {
-        this.scopes[Scopes.API] = require('./api')(this.options);
+        this.scopes[Scopes.API] = require('./api')(this);
+        this.scopes[Scopes.CHANNEL] = this.assertReplyQueue();
+    }
+
+    assertReplyQueue() {
+        const handler= (msg) =>
+            this._resp.emit(msg.properties.correlationId, msg);
+        return (create) => () => create()
+            .then((ch) => ch
+                .assertQueue('', {
+                    durable: false,
+                    exclusive: true,
+                    autoDelete: true
+                })
+                .then(({ queue }) => this._replyTo = queue)
+                .then(() => ch)
+            );
     }
 
 };
